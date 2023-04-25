f405cc38be7dcf37c4dc218e3a0000925d43d80c
Diego <dimoreira.dev@gmail.com>
2023-04-25 13:57:33 -0300

    Finalizando ISSUE-3 (2)

--- src/main/kotlin/com/admin/codedeliverylab/api/utils/UtilHelper.kt
+++ src/main/kotlin/com/admin/codedeliverylab/api/utils/UtilHelper.kt
@@ -3,6 +3,7 @@ package com.admin.codedeliverylab.api.utils
 import com.fasterxml.jackson.core.type.TypeReference
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.KotlinModule
+import java.security.MessageDigest
 import kotlin.reflect.KClass
 
 
@@ -23,3 +24,8 @@ inline fun <reified T : Any, reified R : Any> mapList(list: List<T>, clazz: KCla
     return mapper.convertValue(list, typeReference)
 }
 
+fun hashMD5(senha: String): String {
+    val md = MessageDigest.getInstance("MD5")
+    val digest = md.digest(senha.toByteArray())
+    return digest.fold("") { str, it -> str + "%02x".format(it) }
+}
